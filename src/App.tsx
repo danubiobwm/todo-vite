73e419e6cd4f008c332395f994c018c89aea85c7
José Danubio de Araujo <danubio.bwm@gmail.com>
2026-01-01 19:07:47 -0300

    commit: todo vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Text from "./components/text";
+
+export function App() {
+  return (
+    <>
+      <Text variant="body-md-bold" className="text-pink-base">
+        Ola mundo
+      </Text>
+    </>
+  );
+}
+
+export default App;
